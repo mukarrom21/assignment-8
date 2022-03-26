b16ec11a4fc905697eb22634795594b6ab25d0ab
Mukarrom <mukarrom21@yahoo.com>
2022-03-26 19:42:30 +0600

    randomly select and show in ui

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Cart.css'
 
 const Cart = ({cart}) => {
+    const [product, setProduct] = useState(0)
+    //random number generate
+    
+    const random=()=>{
+        let num = Math.floor((Math.random()*3));
+        const chooseOne = cart[num].name;
+        setProduct(chooseOne)
+    }
+
+    
     
     return (
         <div className='cart'>
             {
-                cart.map(phone=><p>{phone.name}</p>)
+                cart.map(phone=><div key={phone.id}>
+                    <p>{phone.name}</p>
+                    </div>)
             }
-            <button>Choose one for me</button>
+            <button onClick={random}>Choose one for me</button>
             <br />
             <button>Reset</button>
+            <p>{product}</p>
         </div>
     );
 };
